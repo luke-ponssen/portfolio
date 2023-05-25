9bb0fcb37c2daa0dc265d437ae16b819a99b16bf
lukep <luke.ponssen@icloud.com>
2023-05-25 16:01:02 -0700

    delete comments

--- main.js
+++ main.js
@@ -1,40 +1,6 @@
 import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
 //import {OrbitControls} from 'https://unpkg.com/three@0.137.0/examples/jsm/controls/OrbitControls.js';
 
-// const canvas = document.querySelector('.webgl')
-// const scene = new THREE.Scene()
-
-// const geometry = new THREE.BoxGeometry(1,1,1)
-// const material = new THREE.MeshBasicMaterial({
-//   color: 0x00ff00
-// })
-// const mesh = new THREE.Mesh(geometry, material)
-// scene.add(mesh)
-
-
-// const sizes = {
-//   width: window.innerWidth,
-//   height: window.innerHeight
-// }
-
-// const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height, 0.1, 100)
-// camera.position.set(2, 2, 5)
-// scene.add(camera)
-
-// const renderer = new THREE.WebGL1Renderer({
-//   canvas: canvas
-// })
-
-// renderer.setSize(sizes.width, sizes.height)
-// renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-// renderer.shadowMap.enabled = true
-// function animate() {
-//   requestAnimationFrame(animate)
-//   renderer.render(scene, camera)
-// }
-
-// animate()
-
 // Create the scene
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
@@ -55,7 +21,7 @@ const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
 
 // Add a background
-const spaceTexture = new THREE.TextureLoader().load('./images/space.jpg');
+const spaceTexture = new THREE.TextureLoader().load('./images/aspace.jpg');
 scene.background = spaceTexture;
 
 // Add a torus
